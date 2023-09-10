e2ccfb6b04cee1faf0525aec43579268e1864974
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-09-10 15:27:45 +0200

    extra-profilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
-import Home from "./component/Home";
+// import Home from "./component/Home";
+import Profile from "./component/Profile";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Profile />
     </div>
   );
 }
